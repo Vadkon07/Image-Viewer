34cf76458d454f35e53a9ddaa74e6fedefc09981
Vadkon07 <vadkonbeats@gmail.com>
2024-09-16 16:35:09 +0000

    Update

--- main.py
+++ main.py
@@ -7,19 +7,30 @@ class ImageWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Image Viewer")
+
+        #self.setMaximumWidth(500)
+        #self.setMaximumHeight(500) #in dev
+
         layout = QVBoxLayout()
         self.label = QLabel()
-        pixmap = QPixmap(path) #change to argc
+        pixmap = QPixmap(path)
         self.label.setPixmap(pixmap)
+        self.label.setScaledContents(True)
+
         layout.addWidget(self.label)
         self.setLayout(layout)
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
+        
 
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("ERROR: No path provided")
+        sys.exit(1)
+
+    app = QApplication(sys.argv)
     path = sys.argv[1]
 
-    print(path)
+    print(f"Path to image is {path}")
 
     window = ImageWindow()
     window.show()
